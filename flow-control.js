3bf1861ed631cad952b3808007641d3611a1a71c
Adam Moore <adamjamesmoore@live.co.uk>
2020-11-22 16:45:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,14 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19);{
+  if (age >= 13 && age <= 19){
   return ("You are a teenager!");
   }
 
 }
 
 function teenager(age) {
-  if
+  if (age >= 13 && age <= 19){
+    return ("You are a teenager!");
+  }
 
 }
 
